f788cf8f0b66bea9fe1dbcfe786f78f6ed614cb3
AndreiLambrino <152972541+AndreiLambrino@users.noreply.github.com>
2024-06-11 12:31:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,30 +19,13 @@ import streamlit as st
 from ShellyPy import Shelly
 
 # Creazione di un'istanza del dispositivo Shelly
+shelly_ip = '172.16.10.134'
 shelly = Shelly(shelly_ip)
 
-st.title('Stato della Presa Shelly')
-
-def get_shelly_status():
-    try:
-        relay_status = shelly.get_relay(0)
-        st.write("Relay status retrieved:", relay_status)
-        return relay_status
-    except Exception as e:
-        st.error(f"Errore nel recuperare i dati: {e}")
-        return None
-
-# Debug: Mostra l'IP della Shelly
-st.write(f"Collegamento alla presa Shelly all'indirizzo: {shelly_ip}")
-
-relay_status = get_shelly_status()
-
-if relay_status is not None:
-    if relay_status['ison']:
-        st.success("La presa Shelly è ACCESA.")
-    else:
-        st.warning("La presa Shelly è SPENTA.")
-else:
-    st.write("Nessun dato disponibile.")
+try:
+    relay_status = shelly.get_relay(0)
+    print("Relay status retrieved:", relay_status)
+except Exception as e:
+    print(f"Errore nel recuperare i dati: {e}")
 
 
